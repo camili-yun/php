531ff5cadede8e9965a3d35ff0c35295a2cdac66
camili-yun <160083019+camili-yun@users.noreply.github.com>
2024-02-16 17:57:00 -0600

    Add files via upload

---
+++ parcial1/omegaup/12655.C09.php
@@ -0,0 +1,51 @@
+<html>
+  <head></head>
+  <body>
+    <h1>12655. C09</h1>
+    <table border="1">
+      <tr>
+        <td><h4>Puntos</h4></td>
+        <td>14.96</td>
+        <td><h4>Limite de memoria</h4></td>
+        <td>32 Mib</td>
+      </tr>
+      <tr>
+        <td><h4>Tamaño limite de 
+          entrada(bytes)
+        </h4></td>
+        <td>10 kib</td>
+        
+      </tr>
+    </table>
+    <h2>Descripción</h2>
+    <p>
+     Escribe un programa que solicite las calificaciones de Matemáticas, Español y Cocina y nos de su promedio.
+    </p>
+    <h3>Entrada</h3>
+    <p>
+     Tres números reales
+    </p>
+    <h4>Salida</h4>
+    <p>
+     Un número real representado el promedio de los tres números
+    </p>
+    <h4>Ejemplo</h4>
+    <table border="1">
+      <tr>
+        <td><h5>Entrada</h5></td>
+        <td><h5>Salida</h5></td>
+      </tr>
+      <tr>
+        <td>10 9.5 9.0</td>
+        <td>9.5</td>
+      </tr>
+    </table>
+<?php
+  $n1=10;
+  $n2=9.5;
+  $n3=9.0;
+  $total=($n1+$n2+$n3)/3;
+  echo "EL PROMEDIO FINAL ES: ",$total;
+ ?>
+ </body>
+ </html>
